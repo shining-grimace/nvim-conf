9f61f07747bb637d88b96050aac1e719441d7b7d
Thomas Reichert <thomasr1987@gmail.com>
2025-03-12 22:00:55 +1100

    Add more languages to treesitter, tinker with spacing for non-code languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,20 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "lua", "rust", "wgsl", "ron", "bash", "toml" },
+            ensure_installed = {
+                "lua",
+                "rust",
+                "wgsl",
+                "ron",
+                "bash",
+                "toml",
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+                "json",
+                "yaml"
+            },
             highlight = { enable = true },
             indent = { enable = true }
         })
