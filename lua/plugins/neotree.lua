3372d64c8f4030afe8c45a3c39cdfda680e2991f
Thomas Reichert <thomasr1987@gmail.com>
2025-04-01 06:14:33 +1100

    Update Neotree - open on start, show hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,23 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = false,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim"
     },
     config = function()
+        require('neo-tree').setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true
+                }
+            },
+            hijack_netrw_behavior = 'open_default'
+        })
         vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
     end
 }
