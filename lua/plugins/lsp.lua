62d028ae8c48646fdddc94bb02b08da1f79c1e8c
Thomas Reichert <thomasr1987@gmail.com>
2025-06-19 05:51:05 +1000

    Auto-format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,7 +31,6 @@ return {
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<leader>af', vim.lsp.buf.format, {})
         end
     }
 }
