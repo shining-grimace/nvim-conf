eac7f2f32866f6f4eac2aac8f6b351525406b2e7
Thomas Reichert <thomasr1987@gmail.com>
2025-06-25 07:19:11 +1000

    Bump locked dependencies, update conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,29 +1,27 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-        {
-            '<leader>f',
-            function()
-                require('conform').format { async = true, lsp_format = 'fallback' }
-            end,
-            mode = '',
-            desc = '[F]ormat buffer'
-        }
-    },
-    opts = {
-        notify_on_error = false,
-        format_on_save = function(_)
-            return {
-                timeout_ms = 500,
-                lsp_format = 'fallback'
-            }
-        end,
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            rust = { 'rustfmt', lsp_format = 'fallback' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true }
-        }
-    }
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
 }
