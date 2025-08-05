3877b60c9cfc665b02845fe13a597510bb4a4d18
Thomas Reichert <thomasr1987@gmail.com>
2025-08-06 05:32:26 +1000

    Add folke/lazydev.nvim for insights in Neovim config

--- init.lua
+++ init.lua
@@ -11,9 +11,9 @@ vim.g.have_nerd_font = true
 -- Run `:help option` for info on any of these
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.scrolloff = 8
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
@@ -22,39 +22,39 @@ vim.cmd("set shiftwidth=4")
 
 -- Clipboard: share with OS, set after UiEnter to help startup time
 vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Keymaps: clear search highlights, diagnostics, exit terminal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Window keymaps
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move fox=cus to the left window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move fox=cus to the bottom window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move fox=cus to the lower window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move fox=cus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move fox=cus to the left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move fox=cus to the bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move fox=cus to the lower window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move fox=cus to the upper window" })
 
 -- Flash text being yanked
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,36 +1,46 @@
-
 local lsps = { "lua_ls", "rust_analyzer", "html", "cssls", "ts_ls" }
 
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = lsps
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
-            for k, v in pairs(lsps) do
-                lspconfig[v].setup({
-                    capabilities = capabilities
-                })
-            end
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = lsps,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			for k, v in pairs(lsps) do
+				lspconfig[v].setup({
+					capabilities = capabilities,
+				})
+			end
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
